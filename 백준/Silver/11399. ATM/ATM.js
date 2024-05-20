fe3e7e7f4ce46d0fd75977dd3321f16d84fc0a33
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-20 22:14:01 +0900

    Update ATM.js

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,5 +1,3 @@
-const { count } = require("console");
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().split("\n");
 
