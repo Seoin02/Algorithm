78076a8cce4ab3670a4eea6f13563bd94f395736
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-21 14:09:53 +0900

    Update 회의실 배정.js

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -6,7 +6,7 @@ n = Number(n);
 
 arr = arr.map((line) => line.split(" ").map(Number));
 
-// 끝나는 시간을 기준으로 정렬, 끝나는 시간이 같아면 시작 시간을 기주능로 정렬
+// 끝나는 시간을 기준으로 정렬, 끝나는 시간이 같으면 시작 시간을 기준으로 정렬
 arr.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
 
 let answer = 0;
